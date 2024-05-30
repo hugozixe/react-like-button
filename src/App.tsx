c40835848204662cedece65b00808ffb4cd38d0e
Tae Hee Lee <liver1379@naver.com>
2024-05-30 15:11:22 +0900

    삼항연산자 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,24 @@ import "./App.css";
 function App() {
   const [like, setLike] = useState(false);
 
-  const like반전 = () => setLike((현재like) => !현재like);
-  
+  const 클릭했을때_실행할함수 = () => setLike((prev) => !prev);
 
-  let likeButton = <button onClick={like반전}>좋아요</button>;
-  if (like === true) {
-    likeButton = <button onClick={like반전}>좋아요 취소</button>;
-  }
+  const 좋아요또는취소버튼 = like ? (
+    <button onClick={클릭했을때_실행할함수}>좋아요 취소</button>
+  ) : (
+    <button onClick={클릭했을때_실행할함수}>좋아요</button>
+  )
+  
+  // let likeButton = <button onClick={클릭했을때_실행할함수}>좋아요</button>;
+  // if (like === true) {
+  //   likeButton = <button onClick={클릭했을때_실행할함수}>좋아요 취소</button>;
+  // }
 
   return (
     <div>
       <h1>좋아요 버튼 만들기</h1>
-      {likeButton}
+      {좋아요또는취소버튼}
+      {/* {likeButton} */}
     </div>
   );
 }
