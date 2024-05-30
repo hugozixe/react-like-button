e10d79a8ac5ff3c3b0b1b2a81fe59b3be4077628
Tae Hee Lee <liver1379@naver.com>
2024-05-30 16:33:10 +0900

    버튼에 병렬 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,54 @@ import "./App.css";
 
 function App() {
   const [like, setLike] = useState(false);
+  const [dislike, setDisLike] = useState(false);
 
-  const 클릭했을때_실행할함수 = () => setLike((prev) => !prev);
+  const 좋아요_클릭했을때_실행할함수 = () => {
+    // 여기는 좋아요 버튼을 클릭했을 때 실행되는 코드들인데,
+    // 싫어요 누른 상태라면
+    if (dislike === true) {
+      //싫어요 안 누른 상태로 바꿔야지
+      setDisLike(false);
+    }
+    setLike(!like); //true로 하면 안 됨
+  };
+  const 싫어요_눌렀을때_실행할함수 = () => {
+    // 여기는 싫어요 버튼을 클릭했을 때 실행되는 코드들인데,
+    // 좋아요 누른 상태라면
+    if (like === true) {
+      // 좋아요 안 누른 상태로 바꿔야지
+      setLike(false);
+    }
+    setDisLike(!dislike); //true로 하면 안 됨
+  };
 
   const 좋아요또는취소버튼 = like ? (
-    <button onClick={클릭했을때_실행할함수}>좋아요 취소</button>
+    <button onClick={좋아요_클릭했을때_실행할함수}>좋아요 취소</button>
   ) : (
-    <button onClick={클릭했을때_실행할함수}>좋아요</button>
+    <button onClick={좋아요_클릭했을때_실행할함수}>좋아요</button>
   );
 
-  let 싫어요버튼 = <button onClick={클릭했을때_실행할함수}>싫어요</button>;
-  if (like === true) {
-    싫어요버튼 = <button onClick={클릭했을때_실행할함수}>싫어요 취소</button>;
-  }
+  const 싫어요또는취소버튼 = dislike ? (
+    <button onClick={싫어요_눌렀을때_실행할함수}>싫어요 취소</button>
+  ) : (
+    <button onClick={싫어요_눌렀을때_실행할함수}>싫어요</button>
+  );
 
-  // let likeButton = <button onClick={클릭했을때_실행할함수}>좋아요</button>;
+  // let likeButton = <button onClick={좋아요가_눌렸을때_실행할함수}>좋아요</button>;
   // if (like === true) {
-  //   likeButton = <button onClick={클릭했을때_실행할함수}>좋아요 취소</button>;
+  //   likeButton = <button onClick={좋아요가_눌렸을때_실행할함수}>좋아요 취소</button>;
+  // }
+
+  // let 싫어요버튼 = <button onClick={싫어요가_눌렸을때_실행할함수}>싫어요</button>;
+  // if (like === true) {
+  //   싫어요버튼 = <button onClick={싫어요가_눌렸을때_실행할함수}>싫어요 취소</button>;
   // }
 
   return (
     <div>
       <h1>좋아요/싫어요 버튼 구현</h1>
-      {좋아요또는취소버튼}{싫어요버튼}
+      {좋아요또는취소버튼}
+      {싫어요또는취소버튼}
       {/* {likeButton} */}
     </div>
   );
